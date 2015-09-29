0d9ad4b47ad9e4064e4f38ab46781f1539d270d8
elger <elgervb@gmail.com>
2015-09-29 12:31:09 +0200

    tasks sturcture tests

--- app/tests/gulp/GeneratorTest.php
+++ app/tests/gulp/GeneratorTest.php
@@ -1,15 +1,34 @@
 <?php
-
 namespace tests\gulp;
 
+use gulp\Generator;
 /**
  *
  * @author elger
- *        
+ *       
  */
 class GeneratorTest extends \PHPUnit_Framework_TestCase {
 
-	public function test1(){
-		$this->assertTrue(true);
+	private $generator;
+	
+	protected function setUp(){
+		$this->generator = new Generator();
+	}
+	public function testGetAvaliableTasks(){
+		$tasks = $this->generator->getAvaliableTasks();
+		
+		$this->assertGreaterThanOrEqual(1, $tasks->count());
+	}
+	public function testGetAvaliableTasksStructure(){
+		$tasksObj = $this->generator->getAvaliableTasks();
+	
+		$this->assertTrue($tasksObj->offsetExists('tasks'));
+		
+		$tasks = $tasksObj->offsetGet('tasks');
+		$this->assertTrue(is_array($tasks));
+		$this->assertGreaterThanOrEqual(1, count($tasks));
+		
+		$this->assertArrayHasKey('name', $tasks[0]);
+		$this->assertArrayHasKey('vars', $tasks[0]);
 	}
 }
