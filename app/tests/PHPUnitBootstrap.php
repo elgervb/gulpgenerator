021aca948eca50a32d3bcd7d2d9d1bc2d4be4e61
elger <elgervb@gmail.com>
2015-09-29 12:30:55 +0200

    autoload

--- app/tests/PHPUnitBootstrap.php
+++ app/tests/PHPUnitBootstrap.php
@@ -2,10 +2,16 @@
 function loader($class)
 {
     $file = $class . '.php';
-    if (file_exists(__DIR__ . DIRECTORY_SEPARATOR .$file)) {
-        require_once __DIR__ . DIRECTORY_SEPARATOR . $file;
+    if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . '../lib/' . $file)) {
+        require_once __DIR__ . DIRECTORY_SEPARATOR . '../lib/' . $file;
     }else if(realpath(__DIR__ . '/../' . $file)){
       require_once realpath(__DIR__ . '/../' . $file);
     }
 }
 spl_autoload_register('loader');
+
+include_once 'vendor/autoload.php';
+include_once __DIR__ . '/../../vendor/elgervb/compact/classes/compact/ClassLoader.php';
+compact\ClassLoader::create()
+->addClassPath(__DIR__."/app")
+->addClassPath(__DIR__."/app/lib");
