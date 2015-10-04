cdcbac9e43fa596a37504258677ec91403b19d95
elger <elgervb@gmail.com>
2015-10-04 09:59:41 +0200

    include vendor autoload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 use compact\mvvm\FrontController;
 include_once 'vendor/elgervb/compact/classes/compact/ClassLoader.php';
-// include_once 'vendor/autoload.php';
+include_once 'vendor/autoload.php';
 compact\ClassLoader::create()
     ->addClassPath(__DIR__."/app")
     ->addClassPath(__DIR__."/app/lib");
