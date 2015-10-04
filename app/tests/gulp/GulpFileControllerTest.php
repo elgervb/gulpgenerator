8b3174206d170a0d748bb5ecb8eb4895d7676008
elger <elgervb@gmail.com>
2015-10-04 22:53:07 +0200

    add addtional tests

--- app/tests/gulp/GulpFileControllerTest.php
+++ app/tests/gulp/GulpFileControllerTest.php
@@ -13,6 +13,16 @@ use compact\utils\Random;
 class GulpFileControllerTest extends \PHPUnit_Framework_TestCase {
 
 
+	protected function tearDown(){
+		$_POST = [];
+	}
+	
+	public function testGetPredefinedTasks(){
+		$tasks = GulpfileController::instance()->getPredefinedTasks()->getObject();
+		
+		$this->assertTrue(is_array($tasks->{'tasks'}));
+		$this->assertGreaterThanOrEqual(1, $tasks->{'tasks'});
+	}
 	public function testPost204NoContent(){
 		
 		$response = GulpfileController::instance()->post();
@@ -41,6 +51,27 @@ class GulpFileControllerTest extends \PHPUnit_Framework_TestCase {
 	
 		$this->assertTrue($response instanceof HttpStatus);
 		$this->assertEquals(201, $response->getHttpCode(), "Check the http status code");
+		
+		return $response->getContent()->getObject();
+	}
 	
+	public function testAddTask(){
+		$task = $this->testPost201Created();
+		$this->assertNotEmpty($task->{"guid"});
+		$this->assertTrue(is_array($task->{"tasks"}));
+		$this->assertGreaterThanOrEqual(1, count($task->{'tasks'}));
+		
+		// reset post
+		$_POST = [];
+		
+		// prep task
+		$_POST['type'] = "test";
+		$_POST['name'] = "testName";
+		
+		// add task
+		$response = GulpfileController::instance()->addtask($task->{"guid"});
+		$task = $response->getContent()->getObject();
+		$this->assertEquals($_POST['type'], $task->{"type"});
+		$this->assertEquals($_POST['name'], $task->{'name'});
 	}
 }
