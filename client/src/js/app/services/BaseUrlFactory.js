464ffb61ff418a18ba7b34e061a811e8dd732eaf
elger <elgervb@gmail.com>
2015-10-06 00:02:43 +0200

    connect to the right urkl

--- client/src/js/app/services/BaseUrlFactory.js
+++ client/src/js/app/services/BaseUrlFactory.js
@@ -5,7 +5,7 @@ app.factory('BaseUrl', function($location) {
   var host = $location.host();
   // Check for local
   if (host === 'localhost' || host.substr(0, 6) === '127.0.' || host.substr(0, 7) === '192.168') {
-    return 'http://localhost/gulpgenerator/';
+    return 'http://localhost:4010/';
   } else {
     return '/';
   } 
