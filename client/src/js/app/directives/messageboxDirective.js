269aae01197a7a8d9e03ff49d26be8c122639ca6
elger <elgervb@gmail.com>
2015-10-07 22:19:42 +0200

    use messagebox directive

---
+++ client/src/js/app/directives/messageboxDirective.js
@@ -0,0 +1,53 @@
+/* global angular */
+/**
+ * ## Directive to show a message on screen ##
+ * 
+ * ### options ###
+ * 
+ * ***msg***: The (error) message to show to the user
+ * ***title***: the optional title of the dialog. Defaults to 'Error'
+ *
+ * ### example ###
+ * ```html
+ * <messagebox title="'error'" msg="error"></messagebox>
+ * ```
+ */
+app.directive('messagebox', function messageboxDirective($document) {
+  var ESCAPE_KEY = 27;
+  
+  return {
+    restrict: 'E',
+    scope: {
+      msg: '=',
+      title: '='
+    },
+    link: function(scope, element, attrs) {
+      // Add some defaults, when needed...
+      if (!scope.title) {
+        scope.title = 'Error';
+      }
+      
+      var closeDialog = function(e) {
+        if (scope.msg && ESCAPE_KEY === e.keyCode) {
+          scope.msg = '';
+          scope.$apply();
+        }
+      };
+      
+      $document.on('keyup', closeDialog);
+      
+      scope.$on('$destroy', function() {
+        $document.off('keyup', closeDialog);
+      });
+    
+    },
+    /* jshint multistr: true */
+    /* The actual template */
+    template: '<div class="msgbox page" ng-show="msg">\
+      <button class="close" type="button" ng-click="msg=\'\';">&times;</button>\
+      <h4 class="title">{{title}}</h4>\
+      <p ng-bind-html="msg"></p>\
+    </div>\
+    <div class="overlay" ng-show="msg"></div>'
+  };
+});
