390fd92e37daa68605419e812e645e2c6f2d2767
elger <elgervb@gmail.com>
2015-10-07 21:19:52 +0200

    added extra routes and exposed API.

--- server/app/AppContext.php
+++ server/app/AppContext.php
@@ -6,6 +6,7 @@ use compact\IAppContext;
 use compact\routing\Router;
 use compact\handler\impl\json\JsonHandler;
 use gulp\GulpfileController;
+use compact\handler\impl\json\Json;
 
 /**
  * The Application Context
@@ -14,6 +15,12 @@ use gulp\GulpfileController;
  */
 class AppContext implements IAppContext
 {
+	/**
+	 * Regex to match a guid: 4ddb9da0-a641-471d-a926-221a7a33b0ec
+	 * @var string
+	 */
+	const GUID_REGEX = "[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}";
+	
     /**
      * (non-PHPdoc)
      * 
@@ -21,14 +28,54 @@ class AppContext implements IAppContext
      */
     public function routes(Router $router)
     {
-        $router->add("^/gulpfile$", function ()
-        {
-            return GulpfileController::instance()->post();
-        }, 'POST');
+    	$router->add('^/$', function(){
+    		return new Json([
+				"/" => [
+    				"method" => "GET",
+					"desc" => "Get routing info"
+    			],
+    			"/predefinedtasks" => [
+    				"method" => "GET",
+    				"desc" => "Get all predefined tasks"
+    			],
+    			"/gulpfile" => [
+    				"method" => "POST",
+    				"desc" => "Add a new gulpfile"
+    			],
+    			"/gulpfile/:guid" => [
+					"method" => "GET",
+    				"desc" => "Get an existing gulpfile"
+    			],
+    			"/gulpfile/:guid/generate" => [
+	    			"method" => "GET",
+	    			"desc" => "Generate the gulpfile"
+    			],
+    			"/gulpfile/:guid/tasks" => [
+    				"method" => "PUT",
+    				"desc" => "Add a gulp task to an existing gulpfile"
+    			]
+    		]);
+    	});
         
         $router->add("^/predefinedtasks$", function () {
         	return GulpfileController::instance()->getPredefinedTasks();
         }, 'GET');
+        
+        $router->add("^/gulpfile$", function (){
+        	return GulpfileController::instance()->post();
+        }, 'POST');
+        
+        $router->add("^/gulpfile/(".self::GUID_REGEX.")$", function($guid){
+        	return \gulp\GulpfileController::instance()->get($guid);
+        }, 'GET');
+        
+        $router->add("^/gulpfile/(".self::GUID_REGEX.")/generate$", function($guid){
+        	return \gulp\GulpfileController::instance()->download($guid);
+        }, 'GET');
+        
+        $router->add("^/gulpfile/(".self::GUID_REGEX.")/tasks$", function($guid){
+        	return \gulp\GulpfileController::instance()->addtask($guid);
+        }, 'PUT');
         
         if (Context::get()->isLocal()){
         	Context::get()->http()->getResponse()->setCORSHeaders();
