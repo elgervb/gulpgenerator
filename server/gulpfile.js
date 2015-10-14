0e4c38deab6c61944d0a59f2a626e91aaeb0b6e3
Elger van Boxtel <elgervb@gmail.com>
2015-10-14 16:36:55 +0200

    reverted gulpfile

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -6,24 +6,7 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync');
 
 var reload  = browserSync.reload;
-
-gulp.task('default', ['start']);
-
-gulp.task('php', function() {
-	return require('gulp-connect-php').server({
-		base: './',
-		port: 4011,
-		keepalive: true
-	});
-});
-gulp.task('start',['php', 'watch'], function() {
-    browserSync({
-        proxy: '127.0.0.1:4011',
-        port: 4010,
-        open: false,
-        notify: true
-    });
-});
+ 
 gulp.task('test', function() {
     var options = {debug: false, notify: true, stderr: true};
     gulp.src('phpunit.xml')
@@ -31,23 +14,41 @@ gulp.task('test', function() {
         .on('error', notify.onError(notification('fail', 'phpunit')))
         .pipe(notify(notification('pass', 'phpunit')));
 });
+
+gulp.task('php', function() {
+  return require('gulp-connect-php').server({
+    base: './',
+    port: 4011,
+    keepalive: true
+  });
+});
+gulp.task('browser-sync',['php'], function() {
+    browserSync({
+        proxy: '127.0.0.1:4011',
+        port: 4010,
+        open: false,
+        notify: true
+    });
+});
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch(['/**/*.php'], [reload]);
+});
+
 gulp.task('test:watch', function(){
-	var path = require('path');
+  var path = require('path');
     gulp.watch('./**/*.php')
         .on("change", function(file) {
-        	var filename = path.basename(file.path, '.php');
-        	console.log(filename + ' has changed. Running tests...');
-        	
-        	var options = {debug: false, notify: true, stderr: true, filter: '/.*'+filename+'.*/i'};
+          var filename = path.basename(file.path, '.php');
+          console.log(filename + ' has changed. Running tests...');
+          
+          var options = {debug: false, notify: true, stderr: true, filter: '/.*'+filename+'.*/i'};
             gulp.src('phpunit.xml')
                 .pipe(phpunit('', options))
                 .on('error', notify.onError(notification('fail', 'phpunit')))
                 .pipe(notify(notification('pass', 'phpunit')));
         });
 });
-gulp.task('watch', function(){
-	gulp.watch(['./**/*.php'], [reload]);
-});
+
 function notification(status, pluginName, override) {
     var options = {
         title:   ( status == 'pass' ) ? 'Tests Passed' : 'Tests Failed',
