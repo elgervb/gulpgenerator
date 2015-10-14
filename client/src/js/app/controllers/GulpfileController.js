859fd59e69545feb5a3a90329a3bcb7ec4561823
Elger van Boxtel <elgervb@gmail.com>
2015-10-14 09:12:13 +0200

    sort methods, remove $rootScope and added comments

--- client/src/js/app/controllers/GulpfileController.js
+++ client/src/js/app/controllers/GulpfileController.js
@@ -2,7 +2,7 @@
 /**
  * Main controller
  */
-angular.module('gulpgenerator').controller('GulpfileController', function GulpfileController($scope,  $routeParams, $rootScope, $log, TaskService) {
+angular.module('gulpgenerator').controller('GulpfileController', function GulpfileController($scope,  $routeParams, $log, TaskService) {
 
   TaskService.getGulpfile($routeParams.guid).then(function(gulpfile) {
     $scope.gulpfile = gulpfile;
@@ -13,9 +13,9 @@ angular.module('gulpgenerator').controller('GulpfileController', function Gulpfi
   /**
    * Toggle a task and show the task body
    */
-  $scope.toggle = function(task, force) {
+  $scope.toggle = function(task, forceOpen) {
     if ($scope.editmode){return;}
-    if (!force && $scope.toggled === task) {
+    if (!forceOpen && $scope.toggled === task) {
       delete $scope.toggled;
       delete $scope.editmode;
     } else {
@@ -28,9 +28,21 @@ angular.module('gulpgenerator').controller('GulpfileController', function Gulpfi
    * Switch the app into add mode
    */
   $scope.addMode = function() {
-    $rootScope.$broadcast('ADD-MODE');
+    $scope.$broadcast('ADD-MODE');
   };
   
+  /**
+   * Delete a task from the list
+   */
+  $scope.delete = function(task) {
+    $scope.gulpfile.tasks = $scope.gulpfile.tasks.filter(function(value, index) {
+      return value.type !== task.type && value.name !== task.name;
+    });
+  };
+  
+  /**
+   * Validate the form to make sure that there is only one task with each name
+   */
   $scope.validate = function(form, task) {
     var duplicates = $scope.gulpfile.tasks.filter(function(value, index) {
       return value.name === task.name;
@@ -44,14 +56,8 @@ angular.module('gulpgenerator').controller('GulpfileController', function Gulpfi
   };
   
   /**
-   * Delete a task from the list
+   * Listen to ADD-TASK event. Add a new task to the current gulpfile 
    */
-  $scope.delete = function(task) {
-    $scope.gulpfile.tasks = $scope.gulpfile.tasks.filter(function(value, index) {
-      return value.type !== task.type && value.name !== task.name;
-    });
-  };
-  
   $scope.$on('ADD-TASK', function(event, task) {
     // Add task
     TaskService.addTask($routeParams.guid, task)
