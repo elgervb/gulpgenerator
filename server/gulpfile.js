e9bf5fee5d7be224e1afca739b417c307e3ad340
Elger van Boxtel <elgervb@gmail.com>
2016-01-13 15:59:44 +0100

    unit testing

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,53 +1,77 @@
 /* global __dirname */
 var gulp = require('gulp'),
-    notify  = require('gulp-notify'),
-    phpunit = require('gulp-phpunit'),
-    _       = require('lodash'),
+    notify      = require('gulp-notify'),
+    phpunit     = require('gulp-phpunit'),
+    argv        = require('yargs').argv
+    _           = require('lodash'),
     browserSync = require('browser-sync');
 
 var reload  = browserSync.reload;
- 
+
+/**
+* Start a PHP server. Note that this will require at least PHP 5.4 as it uses the built in server
+*/
+gulp.task('serve', function() {
+	var port = argv.port || 4011
+	return require('gulp-connect-php').server({
+		base: './app',
+		port: port,
+		keepalive: true
+	});
+});
+
+/**
+ * Start a PHP server and connect browser sync to it
+ */
+gulp.task('start', ['serve'], function() {
+	var port = argv.port || 4011
+    browserSync({
+        proxy: '127.0.0.1:'+port,
+        port: 4010,
+        open: false,
+        notify: true
+    });
+    
+    gulp.watch(['/**/*.php'], [reload]);
+});
+
+/**
+ * Run all tests. To run with code coverage, use the --coverage commandline argument
+ */
 gulp.task('test', function() {
-    var options = {debug: false, notify: true, stderr: true};
+    var options = {debug: false, notify: true, stderr: false};
+    
+    if (argv.coverage) {
+    	options.coverageText = 'php://stdout';
+    }
     gulp.src('phpunit.xml')
         .pipe(phpunit('', options))
         .on('error', notify.onError(notification('fail', 'phpunit')))
         .pipe(notify(notification('pass', 'phpunit')));
 });
 
-gulp.task('php', function() {
-  return require('gulp-connect-php').server({
-    base: './',
-    port: 4011,
-    keepalive: true
-  });
-});
-gulp.task('browser-sync',['php'], function() {
-    browserSync({
-        proxy: '127.0.0.1:4011',
-        port: 4010,
-        open: false,
-        notify: true
-    });
-});
-gulp.task('default', ['browser-sync'], function () {
-    gulp.watch(['/**/*.php'], [reload]);
-});
-
+/**
+ * Watch sources and tests for changes and run tests
+ */
 gulp.task('test:watch', function(){
-  var path = require('path');
-    gulp.watch('./**/*.php')
+	var path = require('path');
+    gulp.watch(['./app/**/*.php', './tests/**/*.php'])
         .on("change", function(file) {
-          var filename = path.basename(file.path, '.php');
-          console.log(filename + ' has changed. Running tests...');
-          
-          var options = {debug: false, notify: true, stderr: true, filter: '/.*'+filename+'.*/i'};
+        	var filename = path.basename(file.path, '.php');
+        	console.log(filename + ' has changed. Running tests...');
+        	
+        	var options = {debug: false, notify: true, stderr: true, filter: '/.*'+filename+'.*/i'};
+        	if (argv.coverage) {
+            	options.coverageText = 'php://stdout';
+            }
+        	
             gulp.src('phpunit.xml')
                 .pipe(phpunit('', options))
                 .on('error', notify.onError(notification('fail', 'phpunit')))
                 .pipe(notify(notification('pass', 'phpunit')));
         });
 });
+
 
 function notification(status, pluginName, override) {
     var options = {
