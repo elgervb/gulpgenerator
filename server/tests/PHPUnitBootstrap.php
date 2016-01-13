e1ccb977a7fb56a6055be04a90f8968135936b79
Elger van Boxtel <elgervb@gmail.com>
2016-01-13 16:39:34 +0100

    setup correct autoloading

--- server/tests/PHPUnitBootstrap.php
+++ server/tests/PHPUnitBootstrap.php
@@ -1,13 +1,2 @@
 <?php
-function loader($class)
-{
-    $file = $class . '.php';
-    if (file_exists(__DIR__ . DIRECTORY_SEPARATOR .$file)) {
-        require_once __DIR__ . DIRECTORY_SEPARATOR . $file;
-    }else if(realpath(__DIR__ . '/../' . $file)){
-      require_once realpath(__DIR__ . '/../app/' . $file);
-    }
-}
-spl_autoload_register('loader');
-
 include_once './vendor/autoload.php';
