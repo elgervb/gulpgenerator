45178b4267fcef5f4390c52ca3de26ab054b3035
Elger van Boxtel <elgervb@gmail.com>
2016-01-19 20:41:04 +0100

    add gulpfile service

---
+++ client/src/js/app/modules/settings/services/GulpfileService.js
@@ -0,0 +1,10 @@
+/* global angular */
+angular.module('gulpfile.settings').service('gulpfile', ($http) => {
+  
+  let load = () => {
+    return $http.get({
+      url: 'http://localhost:4011/'
+    });
+  };
+  
+});
